d3c328d011173c899158be223d33bb60e30e8d8c
JoseDavid253 <60952530+JoseDavid253@users.noreply.github.com>
2022-09-12 17:33:30 -0500

    Actualizacion de la calse Proyecto

--- src/main/java/com/example/admproyecto/model/Proyecto.java
+++ src/main/java/com/example/admproyecto/model/Proyecto.java
@@ -1,23 +1,39 @@
 package com.example.admproyecto.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 
 @Entity
 @Table (name = "proyectos")
 public class Proyecto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id_Proyecto")
+	@Column(name = "id_Proyecto")	
 	private Integer id;
 	@Column(nullable = false, length = 50)
 	public String nombre;
 	@Column(nullable = false, length = 15)
 	public String estado;
+	@JsonManagedReference
+    @OneToMany(
+        mappedBy = "proyectoAsociado",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true,
+        fetch = FetchType.LAZY
+    )    
+    private List<Historia> historias;
 	
 	public Proyecto() {
 		
